539a0adbe15639e47bcc14f57352445d81bc2c0b
Bernhard Benke <benke@ultrasmall.at>
2013-08-08 19:24:23 +0200

    Delete config.php

--- textpattern/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$txpcfg['db'] = 'katkre';
-$txpcfg['user'] = 'root';
-$txpcfg['pass'] = 'root';
-$txpcfg['host'] = 'localhost';
-$txpcfg['table_prefix'] = '';
-$txpcfg['txpath'] = '/Users/spuz/Sites/kathi/textpattern';
-$txpcfg['dbcharset'] = 'utf8';
-?>
